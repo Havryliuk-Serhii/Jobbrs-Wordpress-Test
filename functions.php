2e3cfa4641e12fd3182b6c3e2b1c1544003ec996
Serhii Havryliuk <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-03-08 21:58:07 +0200

    add navigation menu

--- functions.php
+++ functions.php
@@ -35,6 +35,95 @@ function jobbrs_scripts() {
 add_action('wp_enqueue_scripts', 'jobbrs_scripts');
 
 /**
+ * Delete the site name at the end of the title
+**/
+add_filter( 'document_title_parts', function( $parts ){
+
+	if( isset($parts['site']) ) unset($parts['site']);
+
+	return $parts;
+});
+
+/** 
+ * Bootstrap Walker Nav menu
+**/
+
+class Bootstrap_Menu_Walker extends Walker_Nav_Menu {
+
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+
+      if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+        $t = '';
+        $n = '';
+      } else {
+        $t = "\t";
+        $n = "\n";
+      }
+      $indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
+
+      $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+      $classes[] = 'menu-item-' . $item->ID;
+
+      // Filters the arguments for a single nav menu item
+      $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
+
+      // Filters the CSS class(es) applied to a menu item's list item element
+      $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+      $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+      // Filters the ID applied to a menu item's list item element
+      $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
+      $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+      $output .= $indent . '<li' . $id . $class_names .'>';
+
+      // it would be better to enter the class in Appearance -> Menus -> Screen Options -> CSS classes
+      // $output .= $indent . '<li' . $id . $class_names .'>';
+      $output .= $indent . '<li class="nav-item">';
+
+      $atts = array();
+      $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+      $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+      $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+      $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+      // Filters the HTML attributes applied to a menu item's anchor element
+      $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+      $attributes = '';
+      foreach ( $atts as $attr => $value ) {
+        if ( ! empty( $value ) ) {
+          $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+          $attributes .= ' ' . $attr . '="' . $value . '"';
+        }
+      }
+      $title = apply_filters( 'the_title', $item->title, $item->ID );
+
+      // Filters a menu item's title
+      $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+
+      $item_output = $args->before;
+      $item_output .= '<a class="nav-link js-scroll-trigger"'. $attributes .'>';
+      $item_output .= $args->link_before . $title . $args->link_after;
+      $item_output .= '</a>';
+      $item_output .= $args->after;
+
+	  $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+}
+/**
+ * Delete menu item class
+**/
+add_filter('nav_menu_item_id', 'filter_menu_id');
+add_filter( 'nav_menu_css_class', 'filter_menu_li' );
+function filter_menu_li(){
+    return array('');   
+}
+function filter_menu_id(){
+    return; 
+}
+
+/**
  *  Pagination
  **/
 
--- header.php
+++ header.php
@@ -22,15 +22,31 @@
     </head>
     <body <?php body_class(); ?>>
         <header class="header" role="banner">
-            <nav class="navbar sticky-top navbar-expand">
-                
+            <nav class="navbar navbar-expand-lg sticky-top" id="nav">
+                <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav"  aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+                </button> 
+                <div id="my-nav" class="collapse navbar-collapse">
+                    <?php 
+                        wp_nav_menu( [
+                              'theme_location' => 'primary',
+                              'container' => false,
+                              'menu_class' => 'menu',
+                              'menu_id' => '',
+                              'fallback_cb' => '__return_false',
+                              'items_wrap' => '<ul id="%1$s" class="navbar-nav mr-auto">%3$s</ul>',
+                              'depth' => 0,
+                              'walker' => new Bootstrap_Menu_Walker(),
+                        ] );
+                    ?>
+                </div>
             </nav>
             <div class="hero">
                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/hero.png" alt="header background">
                 <div class="page-title position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
                     <div class="col-md-5 p-lg-5 mx-auto my-5">
-                        <h1 class="font-weight-normal">Test Blog</h1>
-                        <p class="lead font-weight-normal">News on Modern Video Streaming</p>        
+                        <h1 class="font-weight-normal"><?php echo wp_get_document_title(); ?></h1>
+                        <p class="lead font-weight-normal"><?php the_field('page-description'); ?></p>        
                     </div>
                 </div>      
             </div>
